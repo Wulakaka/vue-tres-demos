07253bbb0ff4717b3cb16a3a5d543ed56babe19a
zhang.weifeng <zhangweifeng@diinom.com>
2026-03-09 15:27:39 +0800

    feat: 增加 motion 示例

--- src/views/VueWelcome/ViewWelcome.vue
+++ src/views/VueWelcome/ViewWelcome.vue
@@ -1,8 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { AnimatePresence, motion } from 'motion-v'
+
+const isVisible = ref(true)
+
+function toggleVisibility() {
+  isVisible.value = !isVisible.value
+}
+</script>
 
 <template>
-  <div class="h-screen flex justify-center items-center">
-    <div class="w-25 h-25 outline outline-amber-200"></div>
+  <div class="h-screen flex justify-center items-center flex-col">
+    <AnimatePresence>
+      <motion.div
+        v-if="isVisible"
+        class="w-25 h-25 bg-amber-200"
+        :exit="{ opacity: 0 }"
+        :initial="{ opacity: 0 }"
+        :animate="{ opacity: 1 }"
+      >
+        <motion.div :exit="{ y: 10 }">123123</motion.div>
+      </motion.div>
+    </AnimatePresence>
+    <button @click="toggleVisibility">Toggle Visibility</button>
   </div>
 </template>
 
